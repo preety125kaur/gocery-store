02b26202816815bca45c8d15ee9c96a7c93ef1c4
Pritam Kaur <kaurpritam125@gmail.com>
2025-05-24 18:26:04 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$host = '127.0.0.1';
+$port = '3307';
+$dbname = 'shop_db';
+$username = 'root';
+$password = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
